b6cc4877ab835e272c99cc9590c36abd1f77ed3a
Bartimeyse <bartimeyse@yandex.by>
2018-02-26 01:23:59 +0300

    Fixed commit (test)

--- src/com/DAO/interfaces/IDAOCriminalCase.java
+++ src/com/DAO/interfaces/IDAOCriminalCase.java
@@ -17,14 +17,6 @@ public interface IDAOCriminalCase {
      * Общая информация дела: номер, статус, даты создания и завершения, расследующий детектив
      * SELECT `criminal_case`.`criminal_case_number`, `criminal_case`.`create_date`, `criminal_case`.`close_date`, `criminal_case`.`closed`, `man`.`man_id`, `man`.`name`, `man`.`surname`, `man`.`birthday`, `man`.`home_address` FROM `criminal_case`, `man` WHERE `criminal_case`.`criminal_case_id` = 2 AND `criminal_case`.`detective_id` = `man`.`man_id`
      */
-    CriminalCase getCriminalCaseWithDetective(int caseID);
-    
-    /*
-    * Получить вообще все дела. Для вывода на экран в соотв. разделе сайта
-    */
-   List<CriminalCase> getAllCrimes();
-    
-=======
     CriminalCase getCriminalCaseWithDetective(long caseID);
 
     /*
@@ -32,44 +24,27 @@ public interface IDAOCriminalCase {
     */
     List<CriminalCase> getAllCrimes();
 
->>>>>>> 8e75430233230b0e9ad7c6b17f7fc7b48600b908
     /*
      * Получить все дела закрытые и раскрытые (Generate report by status of criminal cases)
      * SELECT `criminal_case_id`, `criminal_case_number`, `create_date`, `close_date` FROM `criminal_case` WHERE `closed` = 1 AND `close_date` IS NOT NULL
      */
     List<CriminalCase> getAllClosedSolvedCrimes();
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 8e75430233230b0e9ad7c6b17f7fc7b48600b908
     /*
      * Получить все дела закрытые и не раскрытые (Generate report by status of criminal cases)
      * SELECT `criminal_case_id`, `criminal_case_number`, `create_date` FROM `criminal_case` WHERE `closed` = 1 AND `close_date` IS NULL
      * */
     List<CriminalCase> getAllClosedUnsolvedCrimes();
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 8e75430233230b0e9ad7c6b17f7fc7b48600b908
     /*
      * Получить все дела открытые (Generate report by status of criminal cases)
      * SELECT `criminal_case_id`, `criminal_case_number`, `create_date` FROM `criminal_case` WHERE `closed` = 0
      * */
     List<CriminalCase> getAllOpenCrimes();
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 8e75430233230b0e9ad7c6b17f7fc7b48600b908
     /*
      * Детектив: все расследуемые дела с указанием их номера, статуса, датами создания и завершения (Generate report by detective)
      * SELECT `criminal_case_id`, `criminal_case_number`, `create_date`, `close_date`, `closed` FROM `criminal_case` WHERE `detective_id` = 1
      */
-<<<<<<< HEAD
-    List<CriminalCase> getAllCrimesOfDetective(int detectiveID);
-=======
     List<CriminalCase> getAllCrimesOfDetective(long detectiveID);
->>>>>>> 8e75430233230b0e9ad7c6b17f7fc7b48600b908
 }
